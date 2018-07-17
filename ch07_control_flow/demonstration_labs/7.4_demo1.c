935d0845286b394676ce405f1d15ac8c43929e6c
gtwilborn <wilborn.gt@gmail.com>
2018-07-17 02:08:34 -0500

    reorganize ch.7, add labs, edit syntax

---
+++ ch07_control_flow/demonstration_labs/7.4_demo1.c
@@ -0,0 +1,7 @@
+/*Create a tailored times table
+REQUIREMENTS:
+    - Input an integer "x".
+    - Print the product of x and y when y ranges from 1 through 10.
+    - Exit if the user inputs an integer larger than 10.
+    - Use continue to ignore any zeroes or negative numbers in x yet repeat the loop.
+*/
