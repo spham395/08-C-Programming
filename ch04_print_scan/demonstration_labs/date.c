1dce9cff14979ddf446117a3391acca7658ae2c9
CyberTraining <35738034+CyberTraining@users.noreply.github.com>
2019-02-20 09:32:14 -0600

    Update date.c

--- ch04_print_scan/demonstration_labs/date.c
+++ ch04_print_scan/demonstration_labs/date.c
@@ -2,7 +2,9 @@
 
 int main(void)
 {
-    int month = 0; int day = 0; int year = 0; // Ignore this faux pas
+    int month = 0; 
+    int day = 0; 
+    int year = 0; 
 
     printf("Enter today's date as mm-dd-yyyy \n");
     // _flushall(); // Clears all open input streams, leaving them open * This no longer works. We will go into this more once we learn while loops
