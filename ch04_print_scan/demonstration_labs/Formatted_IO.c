b075a577850e97e8e814baaf45016f950a18642e
CyberTraining <35738034+CyberTraining@users.noreply.github.com>
2019-02-20 09:25:30 -0600

    Update Formatted_IO.c

--- ch04_print_scan/demonstration_labs/Formatted_IO.c
+++ ch04_print_scan/demonstration_labs/Formatted_IO.c
@@ -6,7 +6,7 @@ int main(void)
 	double currentTemp[] = {100.1234, 99.876543, 133.7};
 	int numOfRuns[] = {3, 4, 5};
 	
-	printf("%-12 Test Case %-12 Temp(F) %12# of Runs \n");
+	printf("%-12 Test Case %-12 Temp(F) %-12# of Runs \n");
 	printf("%-12c %-012.2f %-12d\n", testCase[0], currentTemp[0], numOfRuns[0]);
 	printf("%-12c %-012.2f %-12d\n", testCase[1], currentTemp[1], numOfRuns[1]);
 	printf("%-12c %-012.2f %-12d\n", testCase[2], currentTemp[2], numOfRuns[2]);
